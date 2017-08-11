740ffcb11f94ff28b2d4a6dd295a5c06c3a81125
jmsche <contact@jmsche.fr>
2017-08-11 09:44:36 +0200

    Fixed loading for PHP >= 7

--- DependencyInjection/LeaptI18nExtension.php
+++ DependencyInjection/LeaptI18nExtension.php
@@ -2,10 +2,10 @@
 
 namespace Leapt\I18nBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -28,8 +28,10 @@ class LeaptI18nExtension extends Extension
         $container->setParameter('leapt_i18n.locales', $config['locales']);
 
         // Routing
-        $this->addClassesToCompile(array('Leapt\I18nBundle\Routing\I18nRouter'));
-        foreach(array('translation_domain') as $parameter) {
+        if (PHP_VERSION_ID < 70000) {
+            $this->addClassesToCompile(['Leapt\I18nBundle\Routing\I18nRouter']);
+        }
+        foreach (['translation_domain'] as $parameter) {
             $container->setParameter('leapt_i18n.routing.' . $parameter, $config['routing'][$parameter]);
         }
     }
